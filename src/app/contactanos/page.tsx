6f55294ba7b9d1ed69e543eb34e64f5bb75a0d5f
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2025-12-04 10:46:57 -0500

    Updates

--- src/app/contactanos/page.tsx
+++ src/app/contactanos/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./Contactanos.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,6 +10,27 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contacto() {
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [mensaje, setMensaje] = useState("");
+
+  const enviarWhatsApp = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const phoneNumber = "573234372766";
+
+    const text = `Nuevo mensaje desde el formulario:
+Nombre: ${nombre}
+Correo: ${correo}
+Mensaje: ${mensaje}`;
+
+    const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
+      text
+    )}`;
+
+    window.open(url, "_blank");
+  };
+
   return (
     <section className={styles.section}>
       {/* Título grande */}
@@ -53,26 +77,43 @@ export default function Contacto() {
             <FontAwesomeIcon icon={faLocationDot} className={styles.icon} />
             <div>
               <p className={styles.label}>Ubicación:</p>
-              <p className={styles.data}>Calle 98 # 22 – 64 Of 716 · Bogotá</p>
+              <p className={styles.data}>Calle 98 # 22 - 64 Of 716 · Bogotá</p>
             </div>
           </div>
         </div>
 
         {/* Formulario */}
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={enviarWhatsApp}>
           <div>
             <label>Nombre</label>
-            <input type="text" placeholder="Tu nombre completo" />
+            <input
+              type="text"
+              placeholder="Tu nombre completo"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+              required
+            />
           </div>
 
           <div>
             <label>Correo electronico</label>
-            <input type="email" placeholder="Tu correo electronico" />
+            <input
+              type="email"
+              placeholder="Tu correo electronico"
+              value={correo}
+              onChange={(e) => setCorreo(e.target.value)}
+              required
+            />
           </div>
 
           <div>
             <label>Mensaje</label>
-            <textarea placeholder="Escribe un mensaje aqui..." />
+            <textarea
+              placeholder="Escribe un mensaje aqui..."
+              value={mensaje}
+              onChange={(e) => setMensaje(e.target.value)}
+              required
+            />
           </div>
 
           <button type="submit" className={styles.button}>
